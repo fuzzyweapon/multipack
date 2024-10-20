43189fef5c000055f2d454482fdd7b198b94aa6a
Julia <1253399+fuzzyweapon@users.noreply.github.com>
2024-10-20 13:42:00 -0700

    Add click to skip logo intro

--- composeApp/src/desktopMain/kotlin/com/thecryingbeard/App.kt
+++ composeApp/src/desktopMain/kotlin/com/thecryingbeard/App.kt
@@ -3,6 +3,7 @@ package com.thecryingbeard
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
@@ -20,21 +21,32 @@ import multipack.composeapp.generated.resources.crying_beard_logo_bordered
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        var showLogo by remember { mutableStateOf(false) }
+        var fastForward by remember { mutableStateOf(false) }
+
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             LaunchedEffect(Unit) {
-                showContent = true
-                delay(2000) // Keep the logo visible for 2 seconds
-                showContent = false
+                showLogo = true
+                if (!fastForward) {
+                    delay(2000) // Keep the logo visible for 2 seconds
+                }
+                showLogo = false
             }
 
             AnimatedVisibility(
-                visible = showContent,
+                visible = showLogo,
                 enter = androidx.compose.animation.fadeIn(animationSpec = tween(1000)),
                 exit = androidx.compose.animation.fadeOut(animationSpec = tween(3000))
             ) {
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.crying_beard_logo_bordered), contentDescription = "Logo")
+                    Image(
+                        painter = painterResource(Res.drawable.crying_beard_logo_bordered),
+                        contentDescription = "Logo",
+                        modifier = Modifier.clickable {
+                            fastForward = true
+                            showLogo = false
+                        }
+                    )
                 }
             }
         }
